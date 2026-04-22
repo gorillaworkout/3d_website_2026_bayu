70d4365a11224b7b5250d38031f04b89232ab694
cheerleadingindonesia <cheerleadingindonesiaweb@gmail.com>
2026-04-22 09:07:36 +0700

    update

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,28 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'lottie-player': any;
+import * as React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'lottie-player': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        src?: string;
+        background?: string;
+        speed?: string;
+        loop?: boolean;
+        autoplay?: boolean;
+        direction?: string;
+        mode?: string;
+        style?: React.CSSProperties;
+      };
+    }
+  }
+
+  namespace React {
+    namespace JSX {
+      interface IntrinsicElements {
+        'lottie-player': any;
+      }
+    }
   }
 }
+
+export {};
